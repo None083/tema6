cc9819ff549cfb87f02535e6c74704bb7808d5bc
Noelia <perezgonzaleznoelia08@gmail.com>
2023-03-06 14:45:27 +0100

    60323

---
+++ src/main/java/UT0611/Figura.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UT0611;
+
+/**
+ *
+ * @author noelia
+ */
+public abstract class Figura {
+    
+    protected int base;
+    protected int altura;
+
+    public Figura(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public Figura() {
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
+    public abstract void calcularArea();
+
+    @Override
+    public String toString() {
+        return "Figura{" + "base=" + base + ", altura=" + altura + '}';
+    }
+    
+    
+    
+}
